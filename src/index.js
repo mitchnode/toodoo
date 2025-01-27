7be77654f9d377900ed5b2bc681695c29a5193df
mitchnode <70460735+mitchnode@users.noreply.github.com>
2025-01-27 23:19:03 +1100

    Fixed buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
-var home_button = document.getElementById("dashboard")
-home_button.setAttribute("style","background-color:grey;color: white;");
-home_button.addEventListener("click", () => { load_page(home); home_button.setAttribute("style","background-color:grey;color: white;");})
-var menu_button = document.getElementById("lists")
-menu_button.addEventListener("click", () => { load_page(menu); menu_button.setAttribute("style","background-color:grey;color: white;");})
+var dashboard_button = document.getElementById("dashboard")
+dashboard_button.setAttribute("style","background-color:lightgrey;");
+//dashboard_button.addEventListener("click", () => { load_page(dashboard); dashboard_button.setAttribute("style","background-color:grey;color: white;");})
+var lists_button = document.getElementById("lists")
+//lists_button.addEventListener("click", () => { load_page(lists); lists_button.setAttribute("style","background-color:grey;color: white;");})
