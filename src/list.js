a735d285bb302e03f5287280491f23e50385f8ff
mitchnode <70460735+mitchnode@users.noreply.github.com>
2025-01-31 22:32:18 +1100

    Created List class and first draft of list functions

---
+++ src/list.js
@@ -0,0 +1,31 @@
+
+export class List {
+    constructor(listName){
+        this.listName = listName;
+        this.listData = [];
+        this.length = 0;
+    }
+
+    addToList(item){
+        this.listData.push(item);
+        this.listData.sort();
+    }
+
+    removeFromList(item){
+        var index = this.listData.findIndex(item);
+        var temp = this.listData[0];
+        this.listData[0] = this.listData[index];
+        this.listData[index] = temp;
+        this.listData.shift();
+        this.listData.sort();
+        return "Deleted ${item} from ${this.listName} at index ${index}";
+    }
+
+    getLength(){
+        return this.listData.length()
+    }
+
+    getList(){
+        return this.listData;
+    }
+}
--- src/lists.js
+++ src/lists.js
@@ -1,4 +1,5 @@
 export const lists = document.createElement("div");
+import { List } from "./list.js"
 
 var count = 1;
 
@@ -7,11 +8,47 @@ title.textContent = "Lists";
 lists.appendChild(title);
 
 function add_new_list() {
-    const new_list = document.createElement("div");
-    new_list.id = count
-    new_list.className = "list";
-    lists.appendChild(new_list);
-    count += 1;
+    lists.removeChild(new_list)
+    const new_list_name = document.createElement("input")
+    lists.appendChild(new_list_name)
+    const new_list_confirm = document.createElement("button")
+    new_list_confirm.id = "new";
+    new_list_confirm.textContent = "O";
+    new_list_confirm.addEventListener("mouseover", () => { new_list_confirm.setAttribute("style","background-color:coral;");});
+    new_list_confirm.addEventListener("mouseout", () => { new_list_confirm.setAttribute("style","");});
+    new_list_confirm.addEventListener("click", () => { lists.removeChild(new_list_name); lists.removeChild(new_list_confirm); confirm_new_list(new_list_name.value)});
+    lists.appendChild(new_list_confirm)
+    //const new_list = document.createElement("div");
+    //new_list.id = count
+    //new_list.className = "list";
+    //lists.appendChild(new_list);
+    //count += 1;
+}
+
+function confirm_new_list(listName) {
+    const list = new List(listName);
+    const listTitle = document.createElement("h2");
+    listTitle.id = "title";
+    listTitle.textContent = listName;
+    lists.appendChild(listTitle);
+
+    const newItemValue = document.createElement("input")
+    lists.appendChild(newItemValue)
+    const newItemButton = document.createElement("button");
+    newItemButton.id = "new";
+    newItemButton.textContent = "+";
+    newItemButton.addEventListener("mouseover", () => { newItemButton.setAttribute("style","background-color:coral;");});
+    newItemButton.addEventListener("mouseout", () => { newItemButton.setAttribute("style","");});
+    newItemButton.addEventListener("click", () => { newItem(list, newItemValue.value); newItemValue.value = ''});
+    lists.appendChild(newItemButton)
+}
+
+function newItem(list, newItemValue){
+    list.addToList(newItemValue);
+    const item = document.createElement("p");
+    item.id = "item";
+    item.textContent = newItemValue
+    lists.appendChild(item)
 }
 
 const new_list = document.createElement("button");
